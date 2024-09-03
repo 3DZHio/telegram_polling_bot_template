94da1e00da9c78f31507f747f49583b1f1e6fb86
3DZHio <3dzhio@gmail.com>
2024-09-03 22:10:57 +0200

    Update

--- prerequisites.sh
+++ prerequisites.sh
@@ -2,10 +2,10 @@
 
 
 ## Variables ##
-source .env || source .env.example
+source .env 2>/dev/null || source .env.example
 
 
-## ReName .env.example to .env ##
+## ReName .env.example To .env ##
 if [ -f .env.example ]; then
   mv .env.example .env
 fi
@@ -13,7 +13,7 @@ fi
 
 ## GitIgnore ##
 if [ "${SAVE_ENV}" == "1" ]; then
-    sed -i "s/^.env/##.env/" .gitignore
+    sed -i "s/^.env$/##.env/" .gitignore
 fi
 
 
