deb595c24e2387064bca9b93fe30b7a59a256412
3DZHio <3dzhio@gmail.com>
2024-09-05 20:35:10 +0200

    Update

--- src/__main__.py
+++ src/__main__.py
@@ -7,7 +7,7 @@ from aiogram.fsm.storage.redis import RedisStorage
 from redis.asyncio import Redis
 
 from src.bot.handlers import get_routers
-from src.bot.utils.logs import setup_logger
+from src.logs import setup_logger
 from src.config import settings
 from src.database.core import db
 
